e4c088031bd46038416b47c0295863bb3c06d173
Seta Rose <Seta.Rose@scorpion.co>
2024-10-29 19:45:00 -0700

    small global.css update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import React from "react";
+import Logo from "../assets/logo.svg";
 
 export default function Home() {
   return (
-    <section>
-      <p>This is for the first project commit into github repo.</p>
-    </section>
+    <main>
+      <header className="max-w-6xl mx-auto px-4 py-6 sm:px-8">
+        <div className="logo-cntnnr">
+          <Image src={Logo} alt="logo" className="logo" />
+        </div>
+      </header>
+    </main>
   );
 }
